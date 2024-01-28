87269dec63a9c8a1cc4cc00b2bea58ee8db72b50
Виталий Чечиков <tankist635ffss@gmail.com>
2024-01-28 19:01:33 +0300

    добавоение лсновной команды и команды закрытия приложения

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,16 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CV18.Infrastructure.Commands;
+using System.Windows;
 
 namespace CV18.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Свойство названия окна
         private string _Title = "Main window";
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
+        }
+        #endregion
+        #region Команды
+
+        #region Команда закрытия приложения
+        public ICommand CloseApplicationCommand { get; }
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+
+        #endregion
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
         }
     }
 }
