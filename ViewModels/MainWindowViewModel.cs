c5b2aefb37faeb63ebb4732a656792c7a5e2dc51
Виталий Чечиков <tankist635ffss@gmail.com>
2024-01-28 13:23:24 +0300

    добавление связи графики и логики приложения(свойство названия окна)

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CV18.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV18.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Main window";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
